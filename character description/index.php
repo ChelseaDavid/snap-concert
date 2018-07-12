7624a8116701515681684a8dfa4ed8ba646066ae
Chelsea David <cryan17@cnm.edu>
2018-07-12 17:03:23 -0600

    basic persona

---
+++ character description/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Concert Review</title>
+	</head>
+
+	<body>
+		<h1><a href="zach.php">Persona</a></h1>
+		<p>Please follow the link above to learn more about the projected persona for possible concert venue review</p>
+	</body>
+</html>
---
+++ character description/zach.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona</title>
+	</head>
+	<h1>Zach Healey</h1>
+	<img src="zach.jpg" alt="Zach Healey">
+	<p>
+		<strong>Name:</strong> Zach <br>
+		<strong>Gender:</strong> Male<br>
+		<strong>Age:</strong> 37<br>
+		<strong>Marital Status:</strong> Single<br>
+		<strong>Profession:</strong> Manager on Duty at Applebees<br>
+		<strong>Income Range:</strong> $40k - 50k per year
+	</p>
+	<h1>Attitude:</h1>
+	<ul>
+		<li>Zach is someone who wants to spend any and all of his extra time wisely and efficiently.<br>
+			Due to his financial and scheduling conflictions, Zach lkes to research as much as he can<br>
+			about a show's venue prior to attending.</li>
+	</ul>
+	<h1>Likes:</h1>
+	<ul>
+		<li>Hanging out with friends</li>
+		<li>Researching new concert line ups</li>
+		<li>Babysitting his nephew on Sundays</li>
+	</ul>
+	<h1>Needs:</h1>
+	<ul>
+		<li>Zach needs to be able to search times and dates to ensure he will be<br>
+			available for the interested show.</li>
+		<li>Zach needs to be able to view pictures of the inquired venue. </li>
+		<li>Zach needs to be able to view pricing information</li>
+		<li>Zach will also need to view location</li>
+	</ul>
+	<h1>Frustrations:</h1>
+	<ul>
+		<li>Zach does not like wasting his time or money for a concert only to arrive at the venue and find it<br>
+			is too small or run down </li>
+	</ul>
+	<h1>Technology:</h1>
+	<h3>Comfort level :</h3>
+	<ul>
+		<li>Zach is comfortable with technology but not too advanced. He does most searching through<br>
+			his mobile device, so prefers that searches be quick and efficient</li>
+		<li>Has a home office with a desktop E machine</li>
+		<li>Samsung Galaxy S7 Edge</li>
+	</ul>
+</html>
